a1fc52cf2be07e6b1f28f564c36c8b1dfd9fa34d
Shayan Shafaghi <sh.shafaghi@aut.ac.ir>
2019-09-06 16:08:38 +0430

    Exercise 2

---
+++ ex2/sigmoid.m
@@ -0,0 +1,17 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+hold = exp(-z);
+hold = 1 + hold;
+g = 1 ./ hold;
+% =============================================================
+
+end
